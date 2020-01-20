bb282445e9d0546287ba9a8be49fb5a781b75365
NowakArtur97 <nowart97@gmail.com>
2020-01-20 12:47:56 +0100

    Import Maven Project from Spring Initializr

---
+++ src/main/java/com/NowakArtur97/WorldOfManga/WorldOfMangaApplication.java
@@ -0,0 +1,13 @@
+package com.NowakArtur97.WorldOfManga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldOfMangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldOfMangaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NowakArtur97/WorldOfManga/WorldOfMangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NowakArtur97.WorldOfManga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldOfMangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
