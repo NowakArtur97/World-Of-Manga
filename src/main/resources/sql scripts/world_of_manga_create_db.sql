c37ef19c745bc10352ae7655ba7241bc7122d454
NowakArtur97 <nowart97@gmail.com>
2020-01-20 18:47:19 +0100

    Insert example data to database

--- src/main/resources/sql scripts/world_of_manga_create_db.sql
+++ src/main/resources/sql scripts/world_of_manga_create_db.sql
@@ -9,6 +9,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
     `user_id` INT(11) NOT NULL AUTO_INCREMENT,
     `user_name` VARCHAR(40) NOT NULL,
+    `password` varchar(100) NOT NULL,
     `email` VARCHAR(100) NOT NULL,
     `first_name` VARCHAR(40),
     `last_name` VARCHAR(40),
@@ -36,3 +37,12 @@ CREATE TABLE `user_role`(
 	CONSTRAINT `FK_USER_ROLE` FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
 	CONSTRAINT `FK_ROLE_USER` FOREIGN KEY (`role_id`) REFERENCES `role`(`role_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 )  ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=LATIN1;
+
+INSERT INTO `user`(`user_name`, `password`, `email`, `is_enabled`) values 
+('user', '$2a$10$FnkNozkIGR1ec3tsByXTG.PjPlu6Ntj7cW.lgPLIFkigHe6jreSw2', 'user@email.com', '1'),
+('admin', '$2a$10$21qh5sabaXLO/IVFOmNSbOPjLTSpUdjvpD7kiIx3Ckr9NovR7CRZO', 'admin@email.com', '1');
+
+INSERT INTO `role` (name)
+VALUES ('ROLE_USER'), ('ROLE_ADMIN');
+
+INSERT INTO `user_role`(`user_id`, `role_id`) VALUES (1, 1), (1, 2), (2, 1);
