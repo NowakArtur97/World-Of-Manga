8b551adcf7901b94e639f8d4adf13081f5c4bda1
NowakArtur97 <nowart97@gmail.com>
2020-01-20 19:04:47 +0100

    Create User Model Class

---
+++ src/main/java/com/NowakArtur97/WorldOfManga/model/User.java
@@ -0,0 +1,48 @@
+package com.NowakArtur97.WorldOfManga.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	@Setter(value = AccessLevel.PRIVATE)
+	private Long id;
+
+	@Column(name = "user_name")
+	private String userName;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "is_enabled")
+	private boolean isEnabled;
+}
