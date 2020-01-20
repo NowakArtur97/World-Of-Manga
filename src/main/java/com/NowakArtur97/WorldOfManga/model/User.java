0bc2b042cbf707bfa3f81098681f8aff961c49f9
NowakArtur97 <nowart97@gmail.com>
2020-01-20 19:49:36 +0100

    Change User roles Fetch Type to Eager

--- src/main/java/com/NowakArtur97/WorldOfManga/model/User.java
+++ src/main/java/com/NowakArtur97/WorldOfManga/model/User.java
@@ -54,7 +54,7 @@ public class User {
 	@Column(name = "is_enabled")
 	private boolean isEnabled;
 
-	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+	@ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
 			CascadeType.REFRESH })
 	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private final Set<Role> roles = new HashSet<>();
