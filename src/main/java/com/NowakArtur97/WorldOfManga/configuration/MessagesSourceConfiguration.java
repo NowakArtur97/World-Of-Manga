66b7495299c6406e7fc1cef588fcecc9359c2d97
NowakArtur97 <nowart97@gmail.com>
2020-01-24 17:48:01 +0100

    Add Local Resolver Bean in new Internationalization Configuration class

--- src/main/java/com/NowakArtur97/WorldOfManga/configuration/MessagesSourceConfiguration.java
+++ src/main/java/com/NowakArtur97/WorldOfManga/configuration/MessagesSourceConfiguration.java
@@ -1,10 +1,14 @@
 package com.NowakArtur97.WorldOfManga.configuration;
 
+import java.util.Locale;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MessagesSourceConfiguration {
@@ -26,5 +30,14 @@ public class MessagesSourceConfiguration {
 		validatorFactoryBean.setValidationMessageSource(messageSource);
 
 		return validatorFactoryBean;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		
+		return sessionLocaleResolver;
 	}
 }
