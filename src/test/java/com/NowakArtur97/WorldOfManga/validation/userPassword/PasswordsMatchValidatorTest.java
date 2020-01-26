a297d73be2f997994ecf9460789a237429cfd14b
NowakArtur97 <nowart97@gmail.com>
2020-01-26 18:28:16 +0100

    Create test methods for Password Match Validator

---
+++ src/test/java/com/NowakArtur97/WorldOfManga/validation/userPassword/PasswordsMatchValidatorTest.java
@@ -0,0 +1,64 @@
+package com.NowakArtur97.WorldOfManga.validation.userPassword;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.NowakArtur97.WorldOfManga.dto.UserPasswordDTO;
+
+@DisplayName("Passwords Match Validator Tests")
+@Tag("PasswordsMatchValidator_Tests")
+public class PasswordsMatchValidatorTest {
+
+	private Validator validator;
+
+	@BeforeEach
+	public void setUp() {
+
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@Test
+	@DisplayName("when validating correct password shouldn`t have errors")
+	public void when_validating_correct_password_should_not_have_errors() {
+
+		String password = "password1";
+
+		UserPasswordDTO userPasswordDTO = UserPasswordDTO.builder().password(password).matchingPassword(password)
+				.build();
+
+		Set<ConstraintViolation<UserPasswordDTO>> violations = validator.validate(userPasswordDTO);
+
+		assertAll(() -> assertTrue(violations.isEmpty(), () -> "shouldn`t have errors but: "));
+	}
+
+	@Test
+	@DisplayName("when validating not matching passwords should have errors")
+	public void when_validating_not_matching_passwords_should_have_errors() {
+
+		String password1 = "password1";
+		String password2 = "1drowssap";
+
+		UserPasswordDTO userPasswordDTO = UserPasswordDTO.builder().password(password1).matchingPassword(password2)
+				.build();
+
+		Set<ConstraintViolation<UserPasswordDTO>> violations = validator.validate(userPasswordDTO);
+
+		assertAll(() -> assertFalse(violations.isEmpty(), () -> "should have errors but: "));
+	}
+}
