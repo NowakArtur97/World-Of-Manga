bcf95d96471abacbdf50f31abe703652ff165314
NowakArtur97 <nowart97@gmail.com>
2020-01-28 20:24:25 +0100

    Change .gitignore file

--- src/test/java/com/NowakArtur97/WorldOfManga/controller/main/seleniumTests/MainControllerUITest.java
+++ src/test/java/com/NowakArtur97/WorldOfManga/controller/main/seleniumTests/MainControllerUITest.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.NowakArtur97.WorldOfManga.controller.main.seleniumPageObjectModel.MainControllerSeleniumPOM;
+import com.NowakArtur97.WorldOfManga.testUtils.ScreenshotUtil;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -22,6 +23,7 @@ public class MainControllerUITest {
 
 	private WebDriver webDriver;
 	private MainControllerSeleniumPOM mainPage;
+	private ScreenshotUtil screenshotUtil;
 
 	@BeforeAll
 	public static void setUp() {
@@ -34,6 +36,7 @@ public class MainControllerUITest {
 
 		webDriver = new ChromeDriver();
 		mainPage = new MainControllerSeleniumPOM(webDriver);
+		screenshotUtil = new ScreenshotUtil();
 	}
 
 	@AfterEach
@@ -46,9 +49,11 @@ public class MainControllerUITest {
 
 	@Test
 	@DisplayName("when load main page")
-	public void when_load_main_page_should_load_all_page_content() {
+	public void when_load_main_page_should_load_all_page_content() throws InterruptedException {
 
 		mainPage.loadMainView();
+
+		screenshotUtil.takeScreenshot(mainPage.getWebDriver(), "main_page_error");
 
 		assertAll(() -> assertNotNull(mainPage.getHeaderText(), () -> "should load header fragment text"),
 				() -> assertNotNull(mainPage.getFooterText(), () -> "should load footer fragment text"),
