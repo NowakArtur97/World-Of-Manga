4ffab82ff58136df1fb97cc94c7e92e8c4db35c2
NowakArtur97 <nowart97@gmail.com>
2020-02-08 09:50:51 +0100

    Create Login Controller UI Pl Test

---
+++ src/test/java/com/NowakArtur97/WorldOfManga/controller/unloggedUser/seleniumTest/LoginControllerUIPlTest.java
@@ -0,0 +1,102 @@
+package com.NowakArtur97.WorldOfManga.controller.unloggedUser.seleniumTest;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.NowakArtur97.WorldOfManga.controller.main.seleniumPOM.MainControllerSeleniumPOM;
+import com.NowakArtur97.WorldOfManga.controller.unloggedUser.seleniumPOM.LoginControllerSeleniumPOM;
+import com.NowakArtur97.WorldOfManga.service.api.UserService;
+import com.NowakArtur97.WorldOfManga.testUtil.enums.LanguageVersion;
+import com.NowakArtur97.WorldOfManga.testUtil.extension.ScreenshotWatcher;
+import com.NowakArtur97.WorldOfManga.testUtil.selenium.SeleniumUITest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ExtendWith(ScreenshotWatcher.class)
+@TestPropertySource("classpath:/pageContent/messages_pl.properties")
+@DisplayName("Registration Controller UI Pl Tests")
+@Tag("RegistrationControllerUIPl_Tests")
+public class LoginControllerUIPlTest extends SeleniumUITest {
+
+	private LoginControllerSeleniumPOM loginPage;
+
+	private MainControllerSeleniumPOM mainPage;
+
+	@Value("${form.login.badCredentials}")
+	private String badCredentialsMessage;
+
+	@Value("${form.login.accountDisabled}")
+	private String accountDisabledMessage;
+
+	@Value("${header.myMangaWorld}")
+	private String userLoggedInMangaListOption;
+
+	@Value("${header.signOut}")
+	private String userLoggedInSignOutOption;
+
+	@Autowired
+	private UserService userService;
+
+	@BeforeEach
+	public void setupDriver() {
+
+		if (webDriver != null) {
+
+			webDriver.quit();
+		}
+
+		webDriver = new ChromeDriver();
+
+		loginPage = new LoginControllerSeleniumPOM(webDriver);
+		mainPage = new MainControllerSeleniumPOM(webDriver);
+	}
+
+	@Test
+	@DisplayName("when login with bad credentials")
+	public void when_login_with_bad_credentials_should_show_bad_credentials_message() {
+
+		String username = "resu";
+		String password = "resu";
+
+		loginPage.loadLoginView(LanguageVersion.PL);
+
+		loginPage.fillMandatoryLoginFields(username, password);
+
+		assertAll(
+				() -> assertTrue(loginPage.getFormBoxText().contains(badCredentialsMessage),
+						() -> "should show bad credentails message"),
+				() -> assertFalse(userService.isUsernameAlreadyInUse(username),
+						() -> "user with given username shouldn`t exist: " + username));
+	}
+
+	@Test
+	@DisplayName("when correct login")
+	public void when_correct_login_should_sing_in_user() {
+
+		String username = "user";
+		String password = "user";
+
+		loginPage.loadLoginView(LanguageVersion.PL);
+
+		loginPage.fillMandatoryLoginFields(username, password);
+
+		assertAll(
+				() -> assertTrue(userService.isUsernameAlreadyInUse(username),
+						() -> "user with given username should exist: " + username),
+				() -> assertTrue(mainPage.getHeaderText().contains(userLoggedInMangaListOption.toUpperCase()),
+						() -> "should show manga list option"),
+				() -> assertTrue(mainPage.getHeaderText().contains(userLoggedInSignOutOption.toUpperCase()),
+						() -> "should show sign out option"));
+	}
+}
---
+++ src/test/java/com/NowakArtur97/WorldOfManga/controller/user/seleniumTest/LogoutControllerUIEngTest.java
@@ -0,0 +1,5 @@
+package com.NowakArtur97.WorldOfManga.controller.user.seleniumTest;
+
+public class LogoutControllerUIEngTest {
+
+}
