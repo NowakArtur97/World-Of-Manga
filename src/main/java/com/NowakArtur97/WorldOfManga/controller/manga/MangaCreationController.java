f8b688273c7da9ba3157735a976cb5db2b92fe27
NowakArtur97 <nowart97@gmail.com>
2020-02-12 13:53:03 +0100

    Create Post Mapping for adding manga and change page content messages

--- src/main/java/com/NowakArtur97/WorldOfManga/controller/manga/MangaCreationController.java
+++ src/main/java/com/NowakArtur97/WorldOfManga/controller/manga/MangaCreationController.java
@@ -1,8 +1,13 @@
 package com.NowakArtur97.WorldOfManga.controller.manga;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.NowakArtur97.WorldOfManga.dto.MangaDTO;
@@ -11,11 +16,25 @@ import com.NowakArtur97.WorldOfManga.dto.MangaDTO;
 @RequestMapping(path = "/admin")
 public class MangaCreationController {
 
-	@GetMapping(path = "/addManga")
-	public String showRegistrationPage(Model theModel) {
+	@GetMapping(path = "/addOrUpdateManga")
+	public String showAddMangaPage(Model theModel) {
 
 		theModel.addAttribute("mangaDTO", new MangaDTO());
 
 		return "views/manga-form";
+	}
+
+	@PostMapping(path = "/addOrUpdateManga")
+	public String processAddMangaPage(Model theModel, @ModelAttribute("mangaDTO") @Valid MangaDTO mangaDTO,
+			BindingResult result) {
+
+		if (result.hasErrors()) {
+
+			theModel.addAttribute("mangaDTO", mangaDTO);
+
+			return "views/manga-form";
+		}
+
+		return "redirect:/";
 	}
 }
