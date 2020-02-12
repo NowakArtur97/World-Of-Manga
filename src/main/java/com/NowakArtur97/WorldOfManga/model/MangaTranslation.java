e3d15f860bc9104ed5af69b6cae81dc6d33eaa2f
NowakArtur97 <nowart97@gmail.com>
2020-02-12 10:25:25 +0100

    Create Manga Translation model

--- src/main/java/com/NowakArtur97/WorldOfManga/model/MangaTranslation.java
+++ src/main/java/com/NowakArtur97/WorldOfManga/model/MangaTranslation.java
@@ -15,16 +15,22 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "manga", schema = "world_fo_manga")
+@Table(name = "manga_translation", schema = "world_fo_manga")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Manga {
+public class MangaTranslation {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "manga_id")
+	@Column(name = "manga_translation_id")
 	@Setter(value = AccessLevel.PRIVATE)
 	private Long id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "description")
+	private String description;
 }
