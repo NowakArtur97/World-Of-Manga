51befa68cc57d94770c5d11b4f24de10d30d2ada
NowakArtur97 <nowart97@gmail.com>
2020-02-13 16:52:19 +0100

    Create model mapper test for Manga Translation

--- src/test/java/com/NowakArtur97/WorldOfManga/mapper/ModelMapperTest.java
+++ src/test/java/com/NowakArtur97/WorldOfManga/mapper/ModelMapperTest.java
@@ -10,8 +10,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.NowakArtur97.WorldOfManga.dto.MangaTranslationDTO;
 import com.NowakArtur97.WorldOfManga.dto.UserDTO;
 import com.NowakArtur97.WorldOfManga.dto.UserPasswordDTO;
+import com.NowakArtur97.WorldOfManga.model.MangaTranslation;
 import com.NowakArtur97.WorldOfManga.model.User;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,8 +25,8 @@ public class ModelMapperTest {
 	private ModelMapper modelMapper;
 
 	@Test
-	@DisplayName("when map dto to entity")
-	public void when_map_dto_to_entity_should_return_entity() {
+	@DisplayName("when map user dto to entity")
+	public void when_map_user_dto_to_entity_should_return_entity() {
 
 		UserDTO userDTOExpected = UserDTO.builder().username("username").firstName("first name").lastName("last name")
 				.userPasswordDTO(UserPasswordDTO.builder().password("password1").matchingPassword("password1").build())
@@ -51,5 +53,30 @@ public class ModelMapperTest {
 				() -> assertEquals(userExpected.getEmail(), userActual.getEmail(),
 						() -> "should return user with email: " + userExpected.getEmail() + ", but was: "
 								+ userActual.getEmail()));
+	}
+
+	@Test
+	@DisplayName("when map manga translation dto to entity")
+	public void when_map_manga_translation_dto_to_entity_should_return_entity() {
+
+		String title = "Manga title";
+		String description = "Manga description";
+
+		MangaTranslationDTO mangaTranslationDTOExpected = MangaTranslationDTO.builder().title(title)
+				.description(description).build();
+
+		MangaTranslation mangaTranslationExpected = MangaTranslation.builder().title(title).description(description)
+				.build();
+
+		MangaTranslation mangaTranslationActual = modelMapper.map(mangaTranslationDTOExpected, MangaTranslation.class);
+
+		assertAll(
+				() -> assertEquals(mangaTranslationExpected.getTitle(), mangaTranslationActual.getTitle(),
+						() -> "should return manga translation with title: " + mangaTranslationExpected.getTitle()
+								+ ", but was: " + mangaTranslationActual.getTitle()),
+				() -> assertEquals(mangaTranslationExpected.getDescription(), mangaTranslationActual.getDescription(),
+						() -> "should return manga translation with description: "
+								+ mangaTranslationExpected.getDescription() + ", but was: "
+								+ mangaTranslationActual.getDescription()));
 	}
 }
