691bf605ac15520dfab1cc0f78fc0b767b0c04e1
NowakArtur97 <nowart97@gmail.com>
2020-02-17 09:13:43 +0100

    Create Manga Mapper Impl Test class

---
+++ src/test/java/com/NowakArtur97/WorldOfManga/mapper/MangaMapperImplTest.java
@@ -0,0 +1,56 @@
+package com.NowakArtur97.WorldOfManga.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.NowakArtur97.WorldOfManga.dto.MangaDTO;
+import com.NowakArtur97.WorldOfManga.mapper.manga.MangaMapperImpl;
+import com.NowakArtur97.WorldOfManga.model.Author;
+import com.NowakArtur97.WorldOfManga.model.Manga;
+import com.NowakArtur97.WorldOfManga.model.MangaTranslation;
+
+@DisplayName("Manga Mapper Impl Tests")
+@Tag("MangaMapperIml_Test")
+public class MangaMapperImplTest {
+
+	private MangaMapperImpl mangaMapperImpl = new MangaMapperImpl();
+
+	@Test
+	@DisplayName("when map manga dto to entity")
+	public void when_map_manga_dto_to_entity_should_return_manga_entity() {
+
+		MangaTranslation mangaEnTranslationExpected = MangaTranslation.builder().title("English title")
+				.description("English description").build();
+
+		MangaTranslation mangaPlTranslationExpected = MangaTranslation.builder().title("Polish title")
+				.description("Polish description").build();
+
+		Author authorExpected = new Author("FirtName LastName");
+
+		MangaDTO mangaDTOExpected = new MangaDTO();
+
+		Set<MangaTranslation> translationsExpected = new HashSet<>();
+		translationsExpected.add(mangaEnTranslationExpected);
+		translationsExpected.add(mangaPlTranslationExpected);
+
+		Set<Author> authorsExpected = new HashSet<>();
+		authorsExpected.add(authorExpected);
+		mangaDTOExpected.setAuthors(authorsExpected);
+
+		Manga mangaActual = mangaMapperImpl.mapMangaDTOToManga(mangaDTOExpected, translationsExpected);
+
+		assertAll(
+				() -> assertEquals(translationsExpected.size(), mangaActual.getTranslations().size(),
+						() -> "should return manga with two translation, but was: "
+								+ mangaActual.getTranslations().size()),
+				() -> assertEquals(authorsExpected.size(), mangaActual.getAuthors().size(),
+						() -> "should return manga with one author, but was: " + mangaActual.getAuthors().size()));
+	}
+}
