76df69b4e3a7a4801f85292d590fd5bfc7531c92
NowakArtur97 <nowart97@gmail.com>
2020-02-19 13:39:30 +0100

    Add tests for find by id method in Manga Service Impl Test class

--- src/test/java/com/NowakArtur97/WorldOfManga/service/impl/MangaServiceImplTest.java
+++ src/test/java/com/NowakArtur97/WorldOfManga/service/impl/MangaServiceImplTest.java
@@ -3,6 +3,7 @@ package com.NowakArtur97.WorldOfManga.service.impl;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
@@ -24,6 +26,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 
 import com.NowakArtur97.WorldOfManga.dto.MangaDTO;
+import com.NowakArtur97.WorldOfManga.exception.MangaNotFoundException;
 import com.NowakArtur97.WorldOfManga.mapper.manga.MangaMapper;
 import com.NowakArtur97.WorldOfManga.model.Author;
 import com.NowakArtur97.WorldOfManga.model.Manga;
@@ -94,7 +97,7 @@ public class MangaServiceImplTest {
 				() -> verify(mangaMapper, times(1)).mapMangaDTOToManga(mangaDTO, mangaTranslationsExpected),
 				() -> verify(mangaRepository, times(1)).save(mangaActual));
 	}
-	
+
 	@Test
 	@DisplayName("when find all")
 	public void when_find_all_should_return_list_of_mangas() throws IOException {
@@ -121,27 +124,76 @@ public class MangaServiceImplTest {
 		mangaExpected.addTranslation(mangaTranslationEnExpected);
 		mangaExpected.addTranslation(mangaTranslationPlExpected);
 		mangaExpected.setImage(image.getBytes());
-		
+
 		Manga mangaExpected2 = new Manga();
 		mangaExpected.addAuthor(authorExpected);
 		mangaExpected.addTranslation(mangaTranslationEnExpected2);
 		mangaExpected.addTranslation(mangaTranslationPlExpected2);
 		mangaExpected.setImage(image2.getBytes());
-		
+
 		List<Manga> mangasExpected = new ArrayList<>();
 		mangasExpected.add(mangaExpected);
 		mangasExpected.add(mangaExpected2);
-		
+
 		when(mangaRepository.findAll()).thenReturn(mangasExpected);
 
 		List<Manga> mangasActual = mangaService.findAll();
 
-		assertAll(
-				() -> assertEquals(mangasExpected.size(), mangasActual.size(), "should return list with all authors"),
+		assertAll(() -> assertEquals(mangasExpected.size(), mangasActual.size(), "should return list with all authors"),
 				() -> assertTrue(mangasActual.contains(mangaExpected),
 						() -> "should contain author, but was: " + mangasActual.contains(mangaExpected)),
 				() -> assertTrue(mangasActual.contains(mangaExpected2),
 						() -> "should contain author, but was: " + mangasActual.contains(mangaExpected2)),
 				() -> verify(mangaRepository, times(1)).findAll());
+	}
+
+	@Test
+	@DisplayName("when find by id found manga")
+	public void when_find_by_id_found_manga_should_return_manga_with_id() throws IOException, MangaNotFoundException {
+
+		MangaTranslation mangaTranslationEnExpected = MangaTranslation.builder().title("English title")
+				.description("English description").build();
+		MangaTranslation mangaTranslationPlExpected = MangaTranslation.builder().title("Polish title")
+				.description("Polish description").build();
+
+		Author authorExpected = new Author("FirsName LastName");
+
+		MockMultipartFile image = new MockMultipartFile("image.jpg", "file bytes".getBytes());
+
+		Manga mangaExpected = new Manga();
+		mangaExpected.addAuthor(authorExpected);
+		mangaExpected.addTranslation(mangaTranslationEnExpected);
+		mangaExpected.addTranslation(mangaTranslationPlExpected);
+		mangaExpected.setImage(image.getBytes());
+
+		Long id = 1L;
+
+		when(mangaRepository.findById(id)).thenReturn(Optional.of(mangaExpected));
+
+		Manga mangaActual = mangaService.findById(id);
+
+		assertAll(
+				() -> assertEquals(mangaExpected.getTranslations(), mangaActual.getTranslations(),
+						"should return manga with translations"),
+				() -> assertEquals(mangaExpected.getAuthors(), mangaActual.getAuthors(),
+						"should return manga with authors"),
+				() -> assertEquals(mangaExpected.getImage(), mangaActual.getImage(), "should return manga with image"),
+				() -> verify(mangaRepository, times(1)).findById(id));
+	}
+
+	@Test
+	@DisplayName("when find by id but cannot find manga")
+	public void when_find_by_id_bu_cannot_find_manga_should_return_manga_with_id() throws IOException {
+
+		Long id = 0L;
+
+		Class<MangaNotFoundException> expectedException = MangaNotFoundException.class;
+
+		when(mangaRepository.findById(id)).thenReturn(Optional.empty());
+
+		assertAll(
+				() -> assertThrows(expectedException, () -> mangaService.findById(id),
+						() -> "should throw MangaNotFoundException, but nothing was thrown"),
+				() -> verify(mangaRepository, times(1)).findById(id));
 	}
 }
