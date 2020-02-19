905c3240efa0f12c5b8bd7a790590261edfae427
NowakArtur97 <nowart97@gmail.com>
2020-02-19 11:37:05 +0100

    Modify Main Controller tests for veryfying new model attributes in Controller

--- src/main/java/com/NowakArtur97/WorldOfManga/controller/main/MainController.java
+++ src/main/java/com/NowakArtur97/WorldOfManga/controller/main/MainController.java
@@ -11,17 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.LocaleResolver;
 
-import com.NowakArtur97.WorldOfManga.model.Manga;
-import com.NowakArtur97.WorldOfManga.model.MangaTranslation;
 import com.NowakArtur97.WorldOfManga.service.api.MangaService;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping(path = "/")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-@Slf4j
 public class MainController {
 
 	private final MangaService mangaService;
@@ -35,12 +31,6 @@ public class MainController {
 
 		theModel.addAttribute("mangas", mangaService.findAll());
 		theModel.addAttribute("locale", locale);
-
-		for (Manga manga : mangaService.findAll()) {
-			for (MangaTranslation translation : manga.getTranslations()) {
-				log.info("LOCALE: " + translation.getLanguage().getLocale());
-			}
-		}
 
 		return "views/main";
 	}
--- src/test/java/com/NowakArtur97/WorldOfManga/controller/main/MainControllerTest.java
+++ src/test/java/com/NowakArtur97/WorldOfManga/controller/main/MainControllerTest.java
@@ -1,9 +1,17 @@
 package com.NowakArtur97.WorldOfManga.controller.main;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,9 +19,19 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.LocaleResolver;
+
+import com.NowakArtur97.WorldOfManga.model.Author;
+import com.NowakArtur97.WorldOfManga.model.Manga;
+import com.NowakArtur97.WorldOfManga.model.MangaTranslation;
+import com.NowakArtur97.WorldOfManga.service.api.MangaService;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Main Controller Tests")
@@ -25,6 +43,12 @@ public class MainControllerTest {
 	@InjectMocks
 	private MainController mainController;
 
+	@Mock
+	private MangaService mangaService;
+
+	@Mock
+	private LocaleResolver cookieLocaleResolver;
+
 	@BeforeEach
 	public void setUp() {
 
@@ -33,8 +57,38 @@ public class MainControllerTest {
 
 	@Test
 	@DisplayName("when load main page")
-	public void when_load_main_page_should_show_main_page() {
+	public void when_load_main_page_should_show_main_page() throws IOException {
 
-		assertAll(() -> mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("views/main")));
+		MangaTranslation mangaTranslationEnExpected = MangaTranslation.builder().title("English title")
+				.description("English description").build();
+		MangaTranslation mangaTranslationPlExpected = MangaTranslation.builder().title("Polish title")
+				.description("Polish description").build();
+
+		Set<MangaTranslation> mangaTranslationsExpected = new HashSet<>();
+		mangaTranslationsExpected.add(mangaTranslationEnExpected);
+		mangaTranslationsExpected.add(mangaTranslationPlExpected);
+
+		Author authorExpected = new Author("FirsName LastName");
+
+		MockMultipartFile image = new MockMultipartFile("image.jpg", "file bytes".getBytes());
+
+		Locale locale = new Locale("en");
+
+		Manga mangaExpected = new Manga();
+		mangaExpected.addAuthor(authorExpected);
+		mangaExpected.addTranslation(mangaTranslationEnExpected);
+		mangaExpected.addTranslation(mangaTranslationPlExpected);
+		mangaExpected.setImage(image.getBytes());
+
+		List<Manga> mangas = new ArrayList<>();
+		mangas.add(mangaExpected);
+
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get("/").locale(locale)
+				.flashAttr("mangas", mangas).flashAttr("locale", locale);
+
+		when(mangaService.findAll()).thenReturn(mangas);
+
+		assertAll(() -> mockMvc.perform(mockRequest).andExpect(status().isOk()).andExpect(view().name("views/main"))
+				.andExpect(model().attribute("mangas", mangas)));
 	}
 }
