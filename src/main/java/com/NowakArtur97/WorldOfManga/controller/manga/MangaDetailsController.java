cca7ead59dccac7dcc9dbae3bf539d3d7660ab0e
NowakArtur97 <nowart97@gmail.com>
2020-02-21 17:33:12 +0100

    Create Manga Rating Request Mapping

--- src/main/java/com/NowakArtur97/WorldOfManga/controller/manga/MangaDetailsController.java
+++ src/main/java/com/NowakArtur97/WorldOfManga/controller/manga/MangaDetailsController.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.NowakArtur97.WorldOfManga.exception.MangaNotFoundException;
 import com.NowakArtur97.WorldOfManga.service.api.MangaService;
@@ -40,5 +42,16 @@ public class MangaDetailsController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@GetMapping(path = "/rateManga")
+	public String showAddMangaPage(HttpServletRequest request, @RequestParam(name = "id") Long id,
+			@RequestParam(name = "rate") Long rating) throws MangaNotFoundException {
+
+		String referer = request.getHeader("Referer");
+		
+		mangaService.rateManga(id, rating);
+		
+		return "redirect:" + referer;
 	}
 }
