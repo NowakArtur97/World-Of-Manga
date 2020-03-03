04d7b5101fe2f698352bf111ba6caf2e5bb93b56
NowakArtur97 <nowart97@gmail.com>
2020-03-03 18:35:14 +0100

    Refactor Manga Validation Test to use Manga Genres field

--- src/test/java/com/NowakArtur97/WorldOfManga/validation/manga/MangaValidationTest.java
+++ src/test/java/com/NowakArtur97/WorldOfManga/validation/manga/MangaValidationTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import com.NowakArtur97.WorldOfManga.dto.MangaDTO;
 import com.NowakArtur97.WorldOfManga.dto.MangaTranslationDTO;
 import com.NowakArtur97.WorldOfManga.model.Author;
+import com.NowakArtur97.WorldOfManga.model.MangaGenre;
 
 @DisplayName("Manga Validation Tests")
 @Tag("MangaValidation_Tests")
@@ -48,8 +49,11 @@ public class MangaValidationTest {
 		Set<Author> authors = new HashSet<>();
 		authors.add(new Author("FirsName LastName"));
 
+		Set<MangaGenre> genres = new HashSet<>();
+		genres.add(new MangaGenre("genre"));
+
 		MangaDTO mangaDTO = MangaDTO.builder().enTranslation(mangaTranslationEnDTO).plTranslation(mangaTranslationPlDTO)
-				.authors(authors).build();
+				.authors(authors).genres(genres).build();
 
 		Set<ConstraintViolation<MangaDTO>> violations = validator.validate(mangaDTO);
 
@@ -69,8 +73,35 @@ public class MangaValidationTest {
 
 		Set<Author> authors = new HashSet<>();
 
+		Set<MangaGenre> genres = new HashSet<>();
+		genres.add(new MangaGenre("genre"));
+
 		MangaDTO mangaDTO = MangaDTO.builder().enTranslation(mangaTranslationEnDTO).plTranslation(mangaTranslationPlDTO)
-				.authors(authors).build();
+				.authors(authors).genres(genres).build();
+
+		Set<ConstraintViolation<MangaDTO>> violations = validator.validate(mangaDTO);
+
+		assertAll(() -> assertFalse(violations.isEmpty(), () -> "should have violation, but: "), () -> assertEquals(1,
+				violations.size(), () -> "should have one violation, but have: " + violations.size()));
+	}
+
+	@Test
+	@DisplayName("when manga genres are empty")
+	public void when_manga_genres_are_empty_should_have_violations() {
+
+		MangaTranslationDTO mangaTranslationEnDTO = MangaTranslationDTO.builder().title("English title")
+				.description("English title description").build();
+
+		MangaTranslationDTO mangaTranslationPlDTO = MangaTranslationDTO.builder().title("Polish title")
+				.description("Polish description").build();
+
+		Set<Author> authors = new HashSet<>();
+		authors.add(new Author("FirsName LastName"));
+
+		Set<MangaGenre> genres = new HashSet<>();
+
+		MangaDTO mangaDTO = MangaDTO.builder().enTranslation(mangaTranslationEnDTO).plTranslation(mangaTranslationPlDTO)
+				.authors(authors).genres(genres).build();
 
 		Set<ConstraintViolation<MangaDTO>> violations = validator.validate(mangaDTO);
 
