480aa93c70bcb804f565e2f00ff250d9963e1f30
NowakArtur97 <nowart97@gmail.com>
2020-03-10 12:02:36 +0100

    Delete neo4j from project

--- src/main/java/com/NowakArtur97/WorldOfManga/service/impl/UserRegistrationServiceImpl.java
+++ src/main/java/com/NowakArtur97/WorldOfManga/service/impl/UserRegistrationServiceImpl.java
@@ -8,7 +8,6 @@ import com.NowakArtur97.WorldOfManga.dto.UserDTO;
 import com.NowakArtur97.WorldOfManga.exception.RoleNotFoundException;
 import com.NowakArtur97.WorldOfManga.mapper.user.UserMapper;
 import com.NowakArtur97.WorldOfManga.model.User;
-import com.NowakArtur97.WorldOfManga.repository.neo4j.UserNeo4jRepository;
 import com.NowakArtur97.WorldOfManga.service.api.RoleService;
 import com.NowakArtur97.WorldOfManga.service.api.UserRegistrationService;
 import com.NowakArtur97.WorldOfManga.service.api.UserService;
@@ -24,16 +23,13 @@ public class UserRegistrationServiceImpl implements UserRegistrationService {
 
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	private final UserNeo4jRepository userNeo4jRepository;
-
 	@Autowired
 	public UserRegistrationServiceImpl(RoleService roleService, UserMapper userMapper, UserService userService,
-			BCryptPasswordEncoder bCryptPasswordEncoder, UserNeo4jRepository userNeo4jRepository) {
+			BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.roleService = roleService;
 		this.userMapper = userMapper;
 		this.userService = userService;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-		this.userNeo4jRepository = userNeo4jRepository;
 	}
 
 	@Override
@@ -47,11 +43,7 @@ public class UserRegistrationServiceImpl implements UserRegistrationService {
 		String hashedPassword = bCryptPasswordEncoder.encode(userToRegister.getPassword());
 		userToRegister.setPassword(hashedPassword);
 
-		userService.save(userToRegister);
-
-		com.NowakArtur97.WorldOfManga.model.neo4j.User userNeo4j = new com.NowakArtur97.WorldOfManga.model.neo4j.User(
-				userToRegister.getUsername());
-		userNeo4jRepository.save(userNeo4j);
+		userToRegister = userService.save(userToRegister);
 
 		return userToRegister;
 	}
