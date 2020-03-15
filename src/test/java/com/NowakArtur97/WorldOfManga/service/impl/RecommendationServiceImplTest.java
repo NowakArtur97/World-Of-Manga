e51c0ac707480982136a4843286fce943890f462
NowakArtur97 <nowart97@gmail.com>
2020-03-15 19:00:42 +0100

    Add test in Recommendation Service Impl Test for Manga in User List recommendation

--- src/test/java/com/NowakArtur97/WorldOfManga/service/impl/RecommendationServiceImplTest.java
+++ src/test/java/com/NowakArtur97/WorldOfManga/service/impl/RecommendationServiceImplTest.java
@@ -19,6 +19,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 
+import com.NowakArtur97.WorldOfManga.enums.MangaInUserListStatus;
 import com.NowakArtur97.WorldOfManga.model.Manga;
 import com.NowakArtur97.WorldOfManga.model.MangaGenre;
 import com.NowakArtur97.WorldOfManga.model.MangaTranslation;
@@ -233,6 +234,77 @@ public class RecommendationServiceImplTest {
 		mangaExpected2.addTranslation(mangaTranslationPlExpected2);
 
 		userExpected3.addMangaRating(mangaExpected2, mangaRating);
+
+		List<Manga> recommendationsExpected = new ArrayList<>();
+		recommendationsExpected.add(mangaExpected);
+		recommendationsExpected.add(mangaExpected2);
+
+		when(mangaService.findAll()).thenReturn(recommendationsExpected);
+		when(userService.isUserLoggedIn()).thenReturn(true);
+		when(userService.loadLoggedInUsername()).thenReturn(userExpected3);
+
+		List<Manga> recommendationsActual = recommendationService.recommendManga();
+
+		assertAll(
+				() -> assertEquals(1, recommendationsActual.size(),
+						() -> "should return one recommendation, but was: " + recommendationsActual.size()),
+				() -> assertTrue(recommendationsActual.contains(mangaExpected),
+						() -> "should contain manga: " + mangaExpected + ", but was: " + recommendationsActual),
+				() -> assertFalse(recommendationsActual.contains(mangaExpected2),
+						() -> "should not contain manga: " + mangaExpected2 + ", but was: " + recommendationsActual),
+				() -> assertEquals(mangaExpected, recommendationsActual.get(0),
+						() -> "should contain most liked manga: " + mangaExpected + ", but was: "
+								+ recommendationsActual));
+	}
+
+	@Test
+	@DisplayName("when recommend manga for logged in user - manga in user list")
+	public void when_recommend_manga_for_logged_in_user_with_manga_in_list_should_return_unknown_manga()
+			throws IOException {
+
+		User userExpected = User.builder().username("user").firstName("first name").lastName("last name")
+				.password("user").email("user@email.com").isEnabled(true).build();
+
+		User userExpected2 = User.builder().username("user2").firstName("first name").lastName("last name")
+				.password("user").email("user@email.com").isEnabled(true).build();
+
+		User userExpected3 = User.builder().username("user3").firstName("first name").lastName("last name")
+				.password("user").email("user@email.com").isEnabled(true).build();
+
+		MangaTranslation mangaTranslationEnExpected = MangaTranslation.builder().title("English title")
+				.description("English description").build();
+		MangaTranslation mangaTranslationPlExpected = MangaTranslation.builder().title("Polish title")
+				.description("Polish description").build();
+
+		MangaTranslation mangaTranslationEnExpected2 = MangaTranslation.builder().title("English title 2")
+				.description("English description").build();
+		MangaTranslation mangaTranslationPlExpected2 = MangaTranslation.builder().title("Polish title 2")
+				.description("Polish description").build();
+
+		MockMultipartFile image = new MockMultipartFile("image.jpg", "file bytes".getBytes());
+		MockMultipartFile image2 = new MockMultipartFile("image2.jpg", "file bytes".getBytes());
+
+		MangaGenre genreExpected = new MangaGenre("genre en", "genre pl");
+		MangaGenre genreExpected2 = new MangaGenre("genre en 2", "genre pl 2");
+
+		Manga mangaExpected = new Manga();
+		mangaExpected.setImage(image.getBytes());
+		mangaExpected.addGenre(genreExpected);
+		mangaExpected.addTranslation(mangaTranslationEnExpected);
+		mangaExpected.addTranslation(mangaTranslationPlExpected);
+
+		userExpected.addMangaToFavourites(mangaExpected);
+		userExpected2.addMangaToFavourites(mangaExpected);
+
+		MangaInUserListStatus status = MangaInUserListStatus.CURRENTLY_READING;
+
+		Manga mangaExpected2 = new Manga();
+		mangaExpected.setImage(image2.getBytes());
+		mangaExpected2.addGenre(genreExpected2);
+		mangaExpected2.addTranslation(mangaTranslationEnExpected2);
+		mangaExpected2.addTranslation(mangaTranslationPlExpected2);
+
+		userExpected3.addMangaToList(mangaExpected2, status);
 
 		List<Manga> recommendationsExpected = new ArrayList<>();
 		recommendationsExpected.add(mangaExpected);
