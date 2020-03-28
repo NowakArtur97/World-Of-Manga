6c3141b3dc6859ef693ee44be3e6ebf41e341ae6
NowakArtur97 <nowart97@gmail.com>
2020-03-28 11:20:01 +0100

    Craete Replace Underscore Generator in new generator package in test utils

---
+++ src/test/java/com/NowakArtur97/WorldOfManga/testUtil/generator/ReplaceUnderscoreGenerator.java
@@ -0,0 +1,56 @@
+package com.NowakArtur97.WorldOfManga.testUtil.generator;
+
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+
+public class ReplaceUnderscoreGenerator extends ReplaceUnderscores {
+
+	private final String wordEndingTestName = "should";
+
+	private final String signReplacedInMethodName = "_";
+
+	private final String signReplacingInMethodName = " ";
+
+	@Override
+	public String generateDisplayNameForClass(Class<?> testClass) {
+
+		String classMethodName = super.generateDisplayNameForClass(testClass);
+
+		String displayName = addSpacesBetweenWords(classMethodName);
+
+		return displayName;
+	}
+
+	@Override
+	public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
+
+		String testMethodName = testMethod.getName();
+
+		int indexOfShouldWord = testMethodName.indexOf(wordEndingTestName);
+
+		String displayName = testMethodName.substring(0, indexOfShouldWord - 1).replace(signReplacedInMethodName, signReplacingInMethodName);
+
+		return displayName;
+	}
+
+	private String addSpacesBetweenWords(String classMethodName) {
+
+		int wordLength = classMethodName.length();
+
+		StringBuilder result = new StringBuilder();
+		result.append(classMethodName.charAt(0));
+
+		for (int i = 1; i < wordLength; i++) {
+
+			if (Character.isUpperCase(classMethodName.charAt(i))) {
+
+				result.append(' ');
+			}
+
+			result.append(classMethodName.charAt(i));
+		}
+
+		return result.toString();
+	}
+}
