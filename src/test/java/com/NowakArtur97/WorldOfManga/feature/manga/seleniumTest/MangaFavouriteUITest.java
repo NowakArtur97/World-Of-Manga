92be8602b61bcaf5614cbdf50caf5b3594d860fb
NowakArtur97 <nowart97@gmail.com>
2021-03-05 17:49:05 +0100

    Add environment variables to Selenium images in config.yml and reduce the number of tests to check if tests pass on Circle CI

--- src/test/java/com/NowakArtur97/WorldOfManga/feature/manga/seleniumTest/MangaFavouriteUITest.java
+++ src/test/java/com/NowakArtur97/WorldOfManga/feature/manga/seleniumTest/MangaFavouriteUITest.java
@@ -37,7 +37,7 @@ class MangaFavouriteUITest extends SeleniumUITest {
     }
 
     @ParameterizedTest(name = "{index}: Browser: {0} | Language Version: {1}")
-    @CsvSource({"Firefox, ENG", "Firefox, PL"})
+    @CsvSource({"Firefox, ENG", "Firefox, PL", "Chrome, ENG", "Chrome, PL"})
     void when_add_manga_for_the_first_time_on_main_page_should_add_manga_to_favourites(String browserName, String language) {
 
         launchBrowser(browserName, language);
@@ -54,84 +54,84 @@ class MangaFavouriteUITest extends SeleniumUITest {
                 () -> "should show manga with one heart, but was: " + mangaList.getLastMangaCardText()));
     }
 
+//    @ParameterizedTest(name = "{index}: Browser: {0} | Language Version: {1}")
+//    @CsvSource({"Firefox, ENG", "Firefox, PL", "Chrome, ENG", "Chrome, PL"})
+//    void when_add_manga_for_the_first_time_should_show_manga_in_favourites(String browserName, String language) {
+//
+//        launchBrowser(browserName, language);
+//
+//        loginPage.loadLoginView(languageVersion);
+//
+//        loginPage.fillMandatoryLoginFields("user", "user");
+//
+//        String firstMangaTitle = mangaList.getMangaTitle(0);
+//
+//        mangaList.chooseManga(0);
+//
+//        mangaList.addOrRemoveFirstMangaFromFavourites();
+//
+//        mangaList.clickMangaUserListLink();
+//
+//        mangaList.chooseFavouritesManga();
+//
+//        assertAll(
+//                () -> assertTrue(mangaList.getLastMangaCardText().contains(firstMangaTitle),
+//                        () -> "should show new manga: " + firstMangaTitle + " in favourites"),
+//                () -> assertTrue(mangaList.countMangaCards() >= 1, () -> "should show at least one manga"),
+//                () -> assertNotNull(mangaList.getMangaListText(), () -> "should load manga list fragment text"));
+//    }
+//
+//    @ParameterizedTest(name = "{index}: Browser: {0} | Language Version: {1}")
+//    @CsvSource({"Firefox, ENG", "Firefox, PL", "Chrome, ENG", "Chrome, PL"})
+//    void when_remove_manga_from_favourites_on_main_page_should_remove_manga_from_favourites(String browserName, String language) {
+//
+//        launchBrowser(browserName, language);
+//
+//        loginPage.loadLoginView(languageVersion);
+//
+//        loginPage.fillMandatoryLoginFields("user", "user");
+//
+//        mangaList.chooseLastManga();
+//
+//        mangaList.addOrRemoveLastMangaFromFavourites();
+//
+//        mangaList.chooseLastManga();
+//
+//        mangaList.addOrRemoveLastMangaFromFavourites();
+//
+//        assertAll(() -> assertTrue(mangaList.getLastMangaFavouritesCounter().contains("0"),
+//                () -> "should show manga with zero hearts, but was: " + mangaList.getLastMangaFavouritesCounter()));
+//    }
+//
+//    @ParameterizedTest(name = "{index}: Browser: {0} | Language Version: {1}")
+//    @CsvSource({"Firefox, ENG", "Firefox, PL", "Chrome, ENG", "Chrome, PL"})
+//    void when_remove_manga_from_favourites_should_not_show_manga_in_list(String browserName, String language) {
+//
+//        launchBrowser(browserName, language);
+//
+//        loginPage.loadLoginView(languageVersion);
+//
+//        loginPage.fillMandatoryLoginFields("user", "user");
+//
+//        mangaList.chooseLastManga();
+//
+//        mangaList.addOrRemoveLastMangaFromFavourites();
+//
+//        mangaList.chooseLastManga();
+//
+//        mangaList.addOrRemoveLastMangaFromFavourites();
+//
+//        mangaList.clickMangaUserListLink();
+//
+//        mangaList.chooseFavouritesManga();
+//
+//        assertAll(
+//                () -> assertEquals(0, mangaList.countMangaCards(), () -> "should not show any manga"),
+//                () -> assertNotNull(mangaList.getMangaListText(), () -> "should load manga list fragment text"));
+//    }
+
     @ParameterizedTest(name = "{index}: Browser: {0} | Language Version: {1}")
-    @CsvSource({"Firefox, ENG", "Firefox, PL"})
-    void when_add_manga_for_the_first_time_should_show_manga_in_favourites(String browserName, String language) {
-
-        launchBrowser(browserName, language);
-
-        loginPage.loadLoginView(languageVersion);
-
-        loginPage.fillMandatoryLoginFields("user", "user");
-
-        String firstMangaTitle = mangaList.getMangaTitle(0);
-
-        mangaList.chooseManga(0);
-
-        mangaList.addOrRemoveFirstMangaFromFavourites();
-
-        mangaList.clickMangaUserListLink();
-
-        mangaList.chooseFavouritesManga();
-
-        assertAll(
-                () -> assertTrue(mangaList.getLastMangaCardText().contains(firstMangaTitle),
-                        () -> "should show new manga: " + firstMangaTitle + " in favourites"),
-                () -> assertTrue(mangaList.countMangaCards() >= 1, () -> "should show at least one manga"),
-                () -> assertNotNull(mangaList.getMangaListText(), () -> "should load manga list fragment text"));
-    }
-
-    @ParameterizedTest(name = "{index}: Browser: {0} | Language Version: {1}")
-    @CsvSource({"Firefox, ENG", "Firefox, PL"})
-    void when_remove_manga_from_favourites_on_main_page_should_remove_manga_from_favourites(String browserName, String language) {
-
-        launchBrowser(browserName, language);
-
-        loginPage.loadLoginView(languageVersion);
-
-        loginPage.fillMandatoryLoginFields("user", "user");
-
-        mangaList.chooseLastManga();
-
-        mangaList.addOrRemoveLastMangaFromFavourites();
-
-        mangaList.chooseLastManga();
-
-        mangaList.addOrRemoveLastMangaFromFavourites();
-
-        assertAll(() -> assertTrue(mangaList.getLastMangaFavouritesCounter().contains("0"),
-                () -> "should show manga with zero hearts, but was: " + mangaList.getLastMangaFavouritesCounter()));
-    }
-
-    @ParameterizedTest(name = "{index}: Browser: {0} | Language Version: {1}")
-    @CsvSource({"Firefox, ENG", "Firefox, PL"})
-    void when_remove_manga_from_favourites_should_not_show_manga_in_list(String browserName, String language) {
-
-        launchBrowser(browserName, language);
-
-        loginPage.loadLoginView(languageVersion);
-
-        loginPage.fillMandatoryLoginFields("user", "user");
-
-        mangaList.chooseLastManga();
-
-        mangaList.addOrRemoveLastMangaFromFavourites();
-
-        mangaList.chooseLastManga();
-
-        mangaList.addOrRemoveLastMangaFromFavourites();
-
-        mangaList.clickMangaUserListLink();
-
-        mangaList.chooseFavouritesManga();
-
-        assertAll(
-                () -> assertEquals(0, mangaList.countMangaCards(), () -> "should not show any manga"),
-                () -> assertNotNull(mangaList.getMangaListText(), () -> "should load manga list fragment text"));
-    }
-
-    @ParameterizedTest(name = "{index}: Browser: {0} | Language Version: {1}")
-    @CsvSource({"Firefox, ENG", "Firefox, PL"})
+    @CsvSource({"Firefox, ENG", "Firefox, PL", "Chrome, ENG", "Chrome, PL"})
     void when_user_not_logged_rating_manga_should_show_login_form(String browserName, String language) {
 
         launchBrowser(browserName, language);
